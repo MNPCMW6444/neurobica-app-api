9916ddc7ea4538dae284463bf2a60897d438480b
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-20 22:33:10 +0300

    type

--- src/components/pages/User/Signedin/Signedin.tsx
+++ src/components/pages/User/Signedin/Signedin.tsx
@@ -9,15 +9,8 @@ export default function Signedin() {
       results={[
         {
           objectDescription: ["{user} - mongo modal interface"],
-          description:
-            "The login was successfull and you have a jwt as a cookie",
-          cookies: [
-            {
-              name: "jwt",
-              description:
-                "A secure httpOnly cookie that contains the Json Web Token",
-            },
-          ],
+          description: "The token is valid nd the full user object is retuned",
+          cookies: [],
         },
       ]}
       errors={[
@@ -30,38 +23,10 @@ export default function Signedin() {
             "This means the server is obviously up because it responds but one of the Dbs, the main and/or the OC are unreachable",
         },
         {
-          code: 400,
-          messageObject: [
-            '{clientError: "At least one of the fields are missing"}',
-          ],
+          code: 401,
+          messageObject: ['{clientError: "Unauthorized"}'],
           description:
-            "This means the email, the fullname, the password or the passwordagain parameter is missing",
-        },
-        {
-          code: 400,
-          messageObject: [
-            'clientError: "Password isn\'t strong enough, the value is" + passStrength',
-          ],
-          description: "This means the two passwords aren't the same",
-        },
-        {
-          code: 400,
-          messageObject: ['{clientError: "Passwords doesn\'t match"}'],
-          description:
-            "This means the password doesn't meet the minimum complexity policy - an ID of at least 2 (Medium), also the ID is given",
-        },
-        {
-          code: 400,
-          messageObject: [
-            '{clientError: "An account with this email already exists"}',
-          ],
-          description: "As it is",
-        },
-        {
-          code: 400,
-          messageObject: ['{clientError: "Email already exists"}'],
-          description:
-            "This means that there is already an account using this email",
+            "This means that the request was made without a jwt httpOnly cookie containing a valid token, try signing in and check that your client is setting the cookie`",
         },
       ]}
     />
